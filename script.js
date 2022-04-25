5027a9015baa701a294624b94b465eb05398ab5c
Ne-anna <annabusele@inbox.lv>
2022-04-25 15:03:15 +0300

    Next/previous and text delayed

---
+++ script.js
@@ -0,0 +1,50 @@
+
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel-item');
+const totalSlides = slides.length;
+
+const nextBtn = document.getElementById('carousel_button-next');
+nextBtn.addEventListener('click', moveToNextSlide)
+
+const prevBtn = document.getElementById('carousel_button-previous');
+prevBtn.addEventListener('click', moveToPrevSlide);
+
+function moveToNextSlide() {
+    if(slidePosition === totalSlides-1){
+        slidePosition = 0;
+    }else {
+        slidePosition++;
+    }
+    updateSlidePositionRight();
+ }
+
+function moveToPrevSlide(){
+    if(slidePosition === 0){
+        slidePosition = totalSlides - 1;
+    }else {
+        slidePosition--;
+    }
+    updateSlidePositionLeft();
+}
+
+function updateSlidePositionRight() {
+    for(let slide of slides) {
+        slide.classList.remove('carousel-item__visible-right');
+        slide.classList.remove('carousel-item__visible-left');
+        slide.classList.add('carousel-item__hidden');
+    }
+    slides[slidePosition].classList.add('carousel-item__visible-right');
+    slides[slidePosition].classList.remove('carousel-item__visible-left'); //noņem visible left
+    slides[slidePosition].classList.remove('carousel-item__hidden'); //esošajam slaidam nepiešķir hidden   
+}
+
+function updateSlidePositionLeft() {
+    for(let slide of slides) {
+        slide.classList.remove('carousel-item__visible-left');
+        slide.classList.remove('carousel-item__visible-right');
+        slide.classList.add('carousel-item__hidden');
+    }
+    slides[slidePosition].classList.add('carousel-item__visible-left');
+    slides[slidePosition].classList.remove('carousel-item__visible-right'); //esošajam nepiešķir visible right
+    slides[slidePosition].classList.remove('carousel-item__hidden'); //esošajam slaidam nepiešķir hidden   
+}
